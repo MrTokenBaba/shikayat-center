8e79312c96af39e753f2758dac71627869628072
muhammadbaloch@aydin.edu.tr <muhammadbaloch@aydin.edu.tr>
2025-09-24 17:34:42 +0300

    second update

--- src/components/company/ProfileCard.js
+++ src/components/company/ProfileCard.js
@@ -1,12 +1,13 @@
+/*
 export default function ProfileCard({profile}) {
 
 
     return (
         <div className=" mx-auto bg-[#0f172a] rounded-2xl shadow-lg overflow-hidden">
-            {/* Top Section */}
+            {/!* Top Section *!/}
             <div className="flex items-center justify-between p-6 bg-[#1e293b]">
                 <div className="flex items-center space-x-4">
-                    {/* Avatar */}
+                    {/!* Avatar *!/}
                     <div className="h-16 w-16 rounded-full overflow-hidden border border-gray-600">
                         <img
                             src="https://randomuser.me/api/portraits/women/44.jpg"
@@ -15,7 +16,7 @@ export default function ProfileCard({profile}) {
                         />
                     </div>
 
-                    {/* User Info */}
+                    {/!* User Info *!/}
                     <div>
                         <p className="text-sm text-gray-400">Company Profile ,</p>
                         <h2 className="text-xl font-bold text-white">{profile.username}</h2>
@@ -23,13 +24,13 @@ export default function ProfileCard({profile}) {
                     </div>
                 </div>
 
-                {/* Button */}
+                {/!* Button *!/}
                 <button className="px-4 py-2 text-sm font-medium bg-[#0f172a] text-white rounded-md hover:bg-[#1e293b]">
                     View profile
                 </button>
             </div>
 
-            {/* Stats Section */}
+            {/!* Stats Section *!/}
             <div className="grid grid-cols-3 divide-x divide-gray-700 bg-[#0f172a] text-center">
                 <div className="p-4">
                     <span className="font-bold text-white">12</span>
@@ -47,3 +48,4 @@ export default function ProfileCard({profile}) {
         </div>
     );
 }
+*/
